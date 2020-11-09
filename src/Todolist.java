54c9c3f32376189c7fdfcc045643f0fc21711464
JakubTesar <jakub.tesar@educanet.cz>
2020-11-09 11:00:55 +0100

    uprava 9.11

--- src/Todolist.java
+++ src/Todolist.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Todolist {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("\n" +
                 " _____ ___________ _____   _     _     _   \n" +
                 "|_   _|  _  |  _  \\  _  | | |   (_)   | |  \n" +
@@ -11,54 +12,57 @@ public class Todolist {
                 "  \\_/  \\___/|___/  \\___/  \\_____/_|___/\\__|\n" +
                 "                                           \n" +
                 "                                           \n");
-        Scanner sc = new Scanner(System.in);
         System.out.println("-----------------------------------------------------");
         System.out.println("You can add up to 10 tasks!");
         System.out.println("You have 0/10!");
         System.out.println("-----------------------------------------------------");
         System.out.println("1) Manage tasks");
         System.out.println("2) Exit");
-
-        int pocitanitask =0;
-
-
         String prvnizvoleni = sc.nextLine();
 
-        if (prvnizvoleni.contains("1")) {
-            System.out.println("-----------------------------------------------------\n" +
-                    "Tasks management\n" +
-                    "-----------------------------------------------------");
-            System.out.println("1) Create a new task\n" +
-                    "2) Update a task\n" +
-                    "3) Destroy a task\n" +
-                    "4) Back to main menu");
-            int[] polemanagement = {1, 2, 3, 4};
-            String druhezvoleni = sc.nextLine();
 
-            boolean pridavanitasku = false;
+        for (int j =0; prvnizvoleni.contains("1");) {                //PRVNÍ FOR
 
-            if (druhezvoleni.contains("1")) {
-                System.out.println("Enter name of the new task:");
-                String[] newtask = new String[9];
+            int pocitanitask = 0;
+            int a = 0;
 
-                pocitanitask++;
-                for (int i =0 ; i< newtask.length; i++){
-                newtask[i] = sc.nextLine();
-
+            if (prvnizvoleni.contains("1")) {
                 System.out.println("-----------------------------------------------------\n" +
-                        "You can add up to 10 tasks!\n" +
-                        "You have "+pocitanitask +"/10!\n" +
-                        "#"+pocitanitask+" –" + newtask[i] +
+                        "Tasks management\n" +
                         "-----------------------------------------------------");
-                pridavanitasku= true;
+                System.out.println("1) Create a new task\n" +
+                        "2) Update a task\n" +
+                        "3) Destroy a task\n" +
+                        "4) Back to main menu");
+                int[] polemanagement = {1, 2, 3, 4};
+                String druhezvoleni = sc.nextLine();
+
+
+                if (druhezvoleni.contains("1")) {
+                    System.out.println("Enter name of the new task:");
+                    String[] newtask = new String[10];
+
+
+                    for (int i = 0; i < newtask.length; i++) {                //DRUHÝ FOR
+                        newtask[i] = sc.nextLine();
+                        pocitanitask++;
+                        System.out.println("-----------------------------------------------------\n" +
+                                "You can add up to 10 tasks!\n" +
+                                "You have " + pocitanitask + "/10!\n" +
+                                "#" + a + " – " + newtask[i] +
+                                "\n-----------------------------------------------------");
+                        if (pocitanitask == 10){
+                            break;
+                        }
+                        a++;
+                    }
+                }
+
             }
+            if (prvnizvoleni.contains("2")) {
+                System.exit(0);
             }
 
         }
-        if (prvnizvoleni.contains("2")) {
-            System.exit(0);
-        }
-
-
     }
 }
